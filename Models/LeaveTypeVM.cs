affec0ac492a1129e0e19a3713e2ff29c1a9c842
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2022-06-02 13:08:12 -0400

    - Added automapper to map Model to VM & vice-versa - Worked on controller (mostly mapping) - Added sweetalert for confirm delete - Added font awesome - Added datatables.net

---
+++ Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Required]
+        [Range(0, 25, ErrorMessage = "The default amount of days must be between 0 and 25.")]
+        public int DefaultDays { get; set; }
+    }
+}
